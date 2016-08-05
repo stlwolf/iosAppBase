83a9f473e7fbad31546d544ba4ef609bce7ee1cf
eddy <eddy@stlwolf.com>
2016-08-06 03:03:14 +0900

    設定ファイル追加してみた

---
+++ appBase/Sources/config.swift
@@ -0,0 +1,15 @@
+//
+//  config.swift
+//  appBase
+//
+//  Created by eddy on 2016/06/24.
+//  Copyright © 2016年 eddy. All rights reserved.
+//
+
+import Foundation
+
+
+// MARK: - Color
+
+let MainColorHex = 0x0B1013   // 黒橡
+let KeyColorHex = 0xFCFAF2    // 白練
